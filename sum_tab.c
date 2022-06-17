8568d68442b8347501a8e0df7c6022f3ccf5ae9d
TeddyBeer94 <achenisbest@free.fr>
2022-06-17 11:37:24 +0200

    Ajout de sum_tab

---
+++ sum_tab.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+     void sum_tab_aux(int T1[], int T2[], T , L){
+        for(int i=0 ; i<L ; i=i+1){
+            T[i]=T1[i]+T2[i]
+        }
+        
+     }
+}
